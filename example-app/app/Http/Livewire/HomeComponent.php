82414b0e88e17a69abb188b8e96851afe8b99c55
Your Name <you@example.com>
2021-10-18 09:33:23 +0700

    first commit

---
+++ example-app/app/Http/Livewire/HomeComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HomeComponent extends Component
+{
+    public function index()
+    {
+        return view('livewire.home-component');
+        
+    }
+}
---
+++ example-app/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/index',[HomeComponent::class, 'index']);
+Route::get('/shop',[ShopComponent::class, 'render']);
+Route::get('/cart',[CartComponent::class,'render']);
+Route::get('/checkout',[CheckoutComponent::class,'render']);
+
+
